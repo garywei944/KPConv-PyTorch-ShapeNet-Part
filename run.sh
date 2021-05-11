63f2052a172a766f4a015d3bafe0fea9cfb8d461
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-05-11 15:20:43 -0400

    Update running commands

--- run.sh
+++ run.sh
@@ -6,3 +6,11 @@
 for radius in 0.25 0.15 0.05; do
   python -m src.models.train_shapnet_part --in-radius $radius
 done
+
+wait
+
+for ctg in Car Lamp; do
+  python -m src.models.train_shapnet_part --ctg $ctg --in-radius 0.2
+done
+
+wait
